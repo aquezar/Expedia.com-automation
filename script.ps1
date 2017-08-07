aff08c551a7bcf0a179104297dff4257b4779b55
Vladimir Klovak <vklovak@gmail.com>
2017-08-07 17:14:12 +0300

    powershell script changed

--- script.ps1
+++ script.ps1
@@ -1,5 +1,2 @@
 [xml]$doc = Get-Content TestResult.xml
-$failed = $doc.'test-results'.'test-suite'.results.'test-suite'.results.'test-suite'.results.'test-suite'.results.'test-suite'.results.'test-suite'.results.'test-case'.name
-$pos = $failed.Indexof("(")
-$result = $failed.Substring(0, $pos)
-echo "TestToRun = $result" | Out-File -Encoding "ASCII" result.parameters
+$failed = $doc.'test-results'.'test-suite'.results.'test-suite'.results.'test-suite'.results.'test-suite'.results.'test-suite'.results.'test-suite'.results.'test-case'.name > results.txt
