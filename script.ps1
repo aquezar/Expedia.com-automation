e9ed5bf334448001f19fceeaa4446cd55814b57b
Vladimir Klovak <vklovak@gmail.com>
2017-08-07 18:14:47 +0300

    powershell script changed

--- script.ps1
+++ script.ps1
@@ -1,2 +1,4 @@
 [xml]$doc = Get-Content TestResult.xml
-$failed = $doc.'test-results'.'test-suite'.results.'test-suite'.results.'test-suite'.results.'test-suite'.results.'test-suite'.results.'test-suite'.results.'test-case'.name > results.txt
+$case = $doc.'test-results'.'test-suite'.results.'test-suite'.results.'test-suite'.results.'test-suite'.results.'test-suite'.results.'test-suite'.results.'test-case'
+$target = ($case|where-object {$_.result -eq "Error"})
+$target.name > results.txt
