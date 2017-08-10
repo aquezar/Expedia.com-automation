05dd39452da4eb37cd40bbfdfddf688c1a2be7d4
Vladimir Klovak <vklovak@gmail.com>
2017-08-10 13:25:12 +0300

    script changed to getfailedTests. Changed atribute for selecting failed test cases

--- getFailedTests.ps1
+++ getFailedTests.ps1
@@ -1,4 +1,4 @@
 [xml]$doc = Get-Content TestResult.xml
 $case = $doc.'test-results'.'test-suite'.results.'test-suite'.results.'test-suite'.results.'test-suite'.results.'test-suite'.results.'test-suite'.results.'test-case'
-$target = ($case|where-object {$_.result -eq "Error"})
-$target.name > results.txt
+$target = ($case|where-object {$_.success -eq "False"})
+$target.name > failed_test.txt
