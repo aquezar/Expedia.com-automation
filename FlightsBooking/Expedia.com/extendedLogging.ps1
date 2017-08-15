609603f1a9112b6bc40e358d5e3c1b6a774c1264
Vladimir Klovak <vklovak@gmail.com>
2017-08-15 12:08:12 +0300

    ExtendedLogging param name fixed

--- FlightsBooking/Expedia.com/extendedLogging.ps1
+++ FlightsBooking/Expedia.com/extendedLogging.ps1
@@ -1,6 +1,6 @@
 param([string]$logging="True")
 $config = 'App.config'
 $doc = (Get-Content $config) -as [xml]
-$node = $doc.configuration.appSettings.add | where {$_.key -eq "ExtendedLoging"}
+$node = $doc.configuration.appSettings.add | where {$_.key -eq "ExtendedLogging"}
 $node.value = $logging
 $doc.Save("$pwd\$config")
